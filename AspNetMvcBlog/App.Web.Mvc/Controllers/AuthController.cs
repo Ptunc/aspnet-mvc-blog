a9b05859ca6d2ee6951ddf7aea72025e703a5cc5
Ptunc <spolentunc@gmail.com>
2023-07-21 14:40:05 +0300

    all fixed

---
+++ AspNetMvcBlog/App.Web.Mvc/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string redirectUrl)
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ AspNetMvcBlog/App.Web.Mvc/Controllers/PageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class PageController : Controller
+    {
+        public ActionResult Detail(int id)
+        {
+            return View();
+        }
+
+    }
+}
